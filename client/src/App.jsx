008376b4b39b5966fe1ffb125e75c6aa387798e6
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-07-30 22:39:24 -0600

    updated app.js and built out the files for mutations.js and queries.js in the utils folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,43 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-import Navbar from './components/Navbar.jsx';
+import Navbar from "./components/Navbar";
+
+// Create an HTTP link to the GraphQL server
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Create a middleware to attach the token to each request
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Create the Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
